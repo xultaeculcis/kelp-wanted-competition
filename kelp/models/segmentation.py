9b9d0f8abd239ca1ec2c05a984f10de4a18b0e3f
Michał Wierzbiński <19872529+xultaeculcis@users.noreply.github.com>
2023-12-17 21:48:31 +0100

    Log confusion matrices (#9)
    * Add confusion matrix logging
    * Use `mlflow` auto-generated random run names

--- kelp/models/segmentation.py
+++ kelp/models/segmentation.py
@@ -9,11 +9,13 @@ import torch.nn as nn
 from lightning_fabric.utilities.exceptions import MisconfigurationException
 from lightning_utilities.core.imports import module_available
 from matplotlib import pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
 from torch import Tensor
 from torch.optim import Adam, AdamW
 from torch.optim.lr_scheduler import OneCycleLR
 from torchmetrics import Accuracy, ConfusionMatrix, Dice, F1Score, JaccardIndex, MetricCollection, Precision, Recall
 
+from kelp import consts
 from kelp.data.utils import unbind_samples
 
 
@@ -74,6 +76,7 @@ class KelpForestSegmentationTask(pl.LightningModule):
                     task=self.hparams["objective"],
                     num_classes=self.hyperparams["num_classes"],
                     ignore_index=self.hyperparams["ignore_index"],
+                    average="none",
                 ),
                 "accuracy": Accuracy(
                     task=self.hparams["objective"],
@@ -174,6 +177,47 @@ class KelpForestSegmentationTask(pl.LightningModule):
 
         return model
 
+    def _log_predictions_batch(self, batch: dict[str, Tensor], batch_idx: int, y_hat_hard: Tensor) -> None:
+        # Ensure global step is non-zero -> that we are not running plotting during sanity val step check
+        if batch_idx < 3 and self.global_step > 0 and False:
+            datamodule = self.trainer.datamodule  # type: ignore[attr-defined]
+            batch["prediction"] = y_hat_hard
+            for key in ["image", "mask", "prediction"]:
+                batch[key] = batch[key].cpu()
+            for sample in unbind_samples(batch):
+                fig = datamodule.plot(sample, suptitle=f"Tile ID: {sample['tile_id']}")
+                self.logger.experiment.log_figure(  # type: ignore[attr-defined]
+                    run_id=self.logger.run_id,  # type: ignore[attr-defined]
+                    figure=fig,
+                    artifact_file=f"image/{sample['tile_id']}_{self.current_epoch:02d}.jpg",
+                )
+                plt.tight_layout()
+                plt.close(fig)
+
+    def _log_confusion_matrices(self, metrics: dict[str, Tensor], cmap: str = "Blues") -> None:
+        for metric_key, title, matrix_kind in zip(
+            ["val/conf_mtrx", "val/norm_conf_mtrx"],
+            ["Confusion matrix", "Normalized confusion matrix"],
+            ["confusion_matrix", "confusion_matrix_normalized"],
+        ):
+            conf_matrix = metrics.pop(metric_key)
+            fig, axes = plt.subplots(1, 1, figsize=(7, 5))
+            ConfusionMatrixDisplay(
+                confusion_matrix=conf_matrix.detach().cpu().numpy(),
+                display_labels=consts.data.CLASSES,
+            ).plot(
+                colorbar=True,
+                cmap=cmap,
+                ax=axes,
+            )
+            axes.set_title(title)
+            self.logger.experiment.log_figure(  # type: ignore[attr-defined]
+                run_id=self.logger.run_id,  # type: ignore[attr-defined]
+                figure=fig,
+                artifact_file=f"image/{matrix_kind}/{matrix_kind}_{self.current_epoch:02d}.jpg",
+            )
+            plt.close(fig)
+
     def forward(self, *args: Any, **kwargs: Any) -> Any:
         return self.model(*args, **kwargs)
 
@@ -202,32 +246,19 @@ class KelpForestSegmentationTask(pl.LightningModule):
         y = batch["mask"]
         y_hat = self.forward(x)
         y_hat_hard = y_hat.argmax(dim=1)
-
         loss = self.loss(y_hat, y)
-
         self.log("val/loss", loss, on_step=False, on_epoch=True, batch_size=x.shape[0])
         self.val_metrics(y_hat_hard, y)
-
-        # Ensure global step is non-zero -> that we are not running plotting during sanity val step check
-        if batch_idx < 3 and self.global_step > 0 and False:
-            datamodule = self.trainer.datamodule  # type: ignore[attr-defined]
-            batch["prediction"] = y_hat_hard
-            for key in ["image", "mask", "prediction"]:
-                batch[key] = batch[key].cpu()
-            for sample in unbind_samples(batch):
-                fig = datamodule.plot(sample, suptitle=f"Tile ID: {sample['tile_id']}")
-                self.logger.experiment.log_figure(  # type: ignore[attr-defined]
-                    run_id=self.logger.run_id,  # type: ignore[attr-defined]
-                    figure=fig,
-                    artifact_file=f"image/{sample['tile_id']}_{self.current_epoch:02d}.jpg",
-                )
-                plt.close(fig)
+        self._log_predictions_batch(batch, batch_idx, y_hat_hard)
 
     def on_validation_epoch_end(self) -> None:
         metrics = self.val_metrics.compute()
-        metrics.pop("val/per_class_iou")
-        metrics.pop("val/norm_conf_mtrx")
-        metrics.pop("val/conf_mtrx")
+        per_class_iou = metrics.pop("val/per_class_iou")
+        self._log_confusion_matrices(metrics)
+        per_class_iou_score_dict = {
+            f"val/iou_{consts.data.CLASSES[idx]}": iou_score for idx, iou_score in enumerate(per_class_iou)
+        }
+        metrics.update(per_class_iou_score_dict)
         self.log_dict(metrics, on_step=False, on_epoch=True)
         self.val_metrics.reset()
 
@@ -237,10 +268,7 @@ class KelpForestSegmentationTask(pl.LightningModule):
         y = batch["mask"]
         y_hat = self.forward(x)
         y_hat_hard = y_hat.argmax(dim=1)
-
         loss = self.loss(y_hat, y)
-
-        # by default, the test and validation steps only log per *epoch*
         self.log("test_loss", loss, on_step=False, on_epoch=True, batch_size=x.shape[0])
         self.test_metrics(y_hat_hard, y)
 
