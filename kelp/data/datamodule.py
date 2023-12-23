Refactor normalization transforms, fix a few bugs, add dev notes

kelp/data/datamodule.py
@@ -8,7 +8,9 @@ from typing import Any, Callable, Literal
 import kornia.augmentation as K
 import pandas as pd
 import pytorch_lightning as pl
+import torch
 import torchvision.transforms as T
+from kornia.augmentation.base import _AugmentationBase
 from matplotlib import pyplot as plt
 from torch import Tensor
 from torch.utils.data import DataLoader
@@ -16,7 +18,7 @@ from torch.utils.data import DataLoader
 from kelp import consts
 from kelp.consts.data import DATASET_STATS
 from kelp.data.dataset import FigureGrids, KelpForestSegmentationDataset
-from kelp.data.transforms import MinMaxNormalize, PerSampleMinMaxNormalize, PerSampleQuantileMinMaxNormalize
+from kelp.data.transforms import MinMaxNormalize, PerSampleMinMaxNormalize, PerSampleQuantileNormalize
 
 # Filter warning from Kornia's `RandomRotation` as we have no control over it
 warnings.filterwarnings(
@@ -270,16 +272,16 @@ class KelpForestDataModule(pl.LightningDataModule):
         q01 = [val["q01"] for val in band_stats.values()]
         q99 = [val["q99"] for val in band_stats.values()]
         stats = BandStats(
-            mean=Tensor(mean),
-            std=Tensor(std),
-            min=Tensor(vmin),
-            max=Tensor(vmax),
-            q01=Tensor(q01),
-            q99=Tensor(q99),
+            mean=torch.tensor(mean),
+            std=torch.tensor(std),
+            min=torch.tensor(vmin),
+            max=torch.tensor(vmax),
+            q01=torch.tensor(q01),
+            q99=torch.tensor(q99),
         )
         return stats, len(band_stats)
 
-    def resolve_normalization_transform(self) -> Callable[[Tensor], Tensor]:
+    def resolve_normalization_transform(self) -> _AugmentationBase:
         if self.normalization_strategy == "z-score":
             return K.Normalize(self.band_stats.mean, self.band_stats.std)  # type: ignore[no-any-return]
         elif self.normalization_strategy == "min-max":
@@ -287,7 +289,7 @@ class KelpForestDataModule(pl.LightningDataModule):
         elif self.normalization_strategy == "quantile":
             return MinMaxNormalize(min_vals=self.band_stats.q01, max_vals=self.band_stats.q99)
         elif self.normalization_strategy == "per-sample-quantile":
-            return PerSampleQuantileMinMaxNormalize(q_low=0.01, q_high=0.99)
+            return PerSampleQuantileNormalize(q_low=0.01, q_high=0.99)
         elif self.normalization_strategy == "per-sample-min-max":
             return PerSampleMinMaxNormalize()
         else:

kelp/data/transforms.py
@@ -1,46 +1,66 @@
 from __future__ import annotations
 
-from typing import Any
-
+import kornia.augmentation as K
 import numpy as np
 import torch
-from torch import Tensor, nn
+from torch import Tensor
 
 from kelp import consts
 
 
-class MinMaxNormalize(nn.Module):
-    def __init__(self, min_vals: Tensor, max_vals: Tensor, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        self.min = min_vals
-        self.max = max_vals
+class MinMaxNormalize(K.IntensityAugmentationBase2D):
+    def __init__(self, min_vals: Tensor, max_vals: Tensor) -> None:
+        super().__init__(p=1, same_on_batch=True)
+        self.flags = {"mins": min_vals.view(1, -1, 1, 1), "maxs": max_vals.view(1, -1, 1, 1)}
 
-    def forward(self, x: Tensor) -> Tensor:
-        x = (x - self.min) / (self.max - self.min + consts.data.EPS)
-        x = x.clamp(self.min, self.max)
-        return x
+    def apply_transform(
+        self,
+        input: Tensor,
+        params: dict[str, Tensor],
+        flags: dict[str, int],
+        transform: Tensor | None = None,
+    ) -> Tensor:
+        mins = torch.as_tensor(flags["mins"], device=input.device, dtype=input.dtype)
+        maxs = torch.as_tensor(flags["maxs"], device=input.device, dtype=input.dtype)
+        input = input.clamp(mins, maxs)
+        input = (input - mins) / (maxs - mins + consts.data.EPS)
+        return input
 
 
-class PerSampleMinMaxNormalize(nn.Module):
-    def forward(self, x: Tensor) -> Tensor:
-        vmin = torch.amin(x, dim=(2, 3)).unsqueeze(2).unsqueeze(3)
-        vmax = torch.amax(x, dim=(2, 3)).unsqueeze(2).unsqueeze(3)
-        return (x - vmin) / (vmax - vmin + consts.data.EPS)
+class PerSampleMinMaxNormalize(K.IntensityAugmentationBase2D):
+    def __init__(self) -> None:
+        super().__init__(p=1, same_on_batch=True)
+
+    def apply_transform(
+        self,
+        input: Tensor,
+        params: dict[str, Tensor],
+        flags: dict[str, int],
+        transform: Tensor | None = None,
+    ) -> Tensor:
+        vmin = torch.amin(input, dim=(2, 3)).unsqueeze(2).unsqueeze(3)
+        vmax = torch.amax(input, dim=(2, 3)).unsqueeze(2).unsqueeze(3)
+        return (input - vmin) / (vmax - vmin + consts.data.EPS)
 
 
-class PerSampleQuantileMinMaxNormalize(nn.Module):
-    def __init__(self, q_low: float, q_high: float, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        self.q_low = q_low
-        self.q_high = q_high
+class PerSampleQuantileNormalize(K.IntensityAugmentationBase2D):
+    def __init__(self, q_low: float, q_high: float) -> None:
+        super().__init__(p=1, same_on_batch=True)
+        self.flags = {"q_low": q_low, "q_high": q_high}
 
-    def forward(self, x: Tensor) -> Tensor:
-        flattened_sample = x.view(x.shape[0], x.shape[1], -1)
-        vmin = torch.quantile(flattened_sample, self.q_low, dim=2)
-        vmax = torch.quantile(flattened_sample, self.q_high, dim=2)
-        x = (x - vmin) / (vmax - vmin + consts.data.EPS)
-        x = x.clamp(self.min, self.max)
-        return x
+    def apply_transform(
+        self,
+        input: Tensor,
+        params: dict[str, Tensor],
+        flags: dict[str, int],
+        transform: Tensor | None = None,
+    ) -> Tensor:
+        flattened_sample = input.view(input.shape[0], input.shape[1], -1)
+        vmin = torch.quantile(flattened_sample, flags["q_low"], dim=2).unsqueeze(2).unsqueeze(3)
+        vmax = torch.quantile(flattened_sample, flags["q_high"], dim=2).unsqueeze(2).unsqueeze(3)
+        input = input.clamp(vmin, vmax)
+        input = (input - vmin) / (vmax - vmin + consts.data.EPS)
+        return input
 
 
 def min_max_normalize(arr: np.ndarray) -> np.ndarray:  # type: ignore[type-arg]