8dd4ea93b3fc6528099d31509918ddb0e2ccd076
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2024-02-20 09:01:29 +0100

    Refactor a bit

--- kelp/nn/inference/fold_weights.py
+++ kelp/nn/inference/fold_weights.py
@@ -1,26 +1,32 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-fold_scores = [
-    ("fold=0", 0.7110),
-    ("fold=1", 0.7086),
-    ("fold=2", 0.7110),
-    ("fold=3", 0.7139),
-    ("fold=4", 0.7106),
-    ("fold=5", 0.7100),
-    ("fold=6", 0.7119),
-    ("fold=7", 0.7105),
-    ("fold=8", 0.7155),
-    ("fold=9", 0.7047),
-    ("fold=0v2", 0.7135),
-    ("fold=1v2", 0.7114),
-    ("fold=2v2", 0.7094),
-    ("fold=3v2", 0.7133),
-    ("fold=4v2", 0.7106),
-]
-df = pd.DataFrame(fold_scores, columns=["fold", "score"])
 
-scaler = MinMaxScaler()
-norm_scores = scaler.fit_transform(df[["score"]].values)
-df["score_norm"] = norm_scores
-df.to_parquet("scores.parquet")
+def main() -> None:
+    fold_scores = [
+        ("fold=0", 0.7110),
+        ("fold=1", 0.7086),
+        ("fold=2", 0.7110),
+        ("fold=3", 0.7139),
+        ("fold=4", 0.7106),
+        ("fold=5", 0.7100),
+        ("fold=6", 0.7119),
+        ("fold=7", 0.7105),
+        ("fold=8", 0.7155),
+        ("fold=9", 0.7047),
+        ("fold=0v2", 0.7135),
+        ("fold=1v2", 0.7114),
+        ("fold=2v2", 0.7094),
+        ("fold=3v2", 0.7133),
+        ("fold=4v2", 0.7106),
+    ]
+    df = pd.DataFrame(fold_scores, columns=["fold", "score"])
+
+    scaler = MinMaxScaler()
+    norm_scores = scaler.fit_transform(df[["score"]].values)
+    df["score_norm"] = norm_scores
+    df.to_parquet("scores.parquet")
+
+
+if __name__ == "__main__":
+    main()
